7e5ab739f043b6882395f3f726f487f006f08490
kanaru-ssk <2019january.0131@gmail.com>
2022-02-03 15:38:38 +0900

    講師データ反映

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 
-// jsonデータ挿入
+// 学生データ挿入
 fetch('./json/exhibitor.json')
 .then(response => response.json())
 .then(data => insertExhibitor(data));
+
+// 講師データ挿入
+fetch('./json/lecturer.json')
+.then(response => response.json())
+.then(data => insertLecturer(data));
 
 window.onload = () => {
     let isModalShow = false;
@@ -24,16 +29,60 @@ window.onload = () => {
     });
 }
 
-
 const insertExhibitor = (exhibitors) => {
     exhibitors.forEach(exhibitor => {
-        document.getElementById(exhibitor.labo + '-labo').appendChild(createNode(exhibitor.name));
+        document.getElementById(exhibitor.labo + '-labo').appendChild(createExhibitorNode(exhibitor))
     });
 }
 
-const createNode = (_name) => {
-    const name = document.createTextNode(_name);
+const createExhibitorNode = (exhibitor) => {
     const span = document.createElement('span');
+    const name = document.createTextNode(exhibitor.name);
     span.appendChild(name);
     return span;
+}
+
+const insertLecturer = (lecturers) => {
+    lecturers.forEach(lecturer => {
+        document.getElementById('lecturer-profile-ul').appendChild(createLecturerNode(lecturer));
+    });
+}
+
+const createLecturerNode = (lecturer) => {
+    
+    const img = document.createElement('img');
+    img.setAttribute('class', 'lecturer-profile-img');
+    img.setAttribute('src', './img/lecturers/' + lecturer.img_name);
+
+    const roman = document.createTextNode(lecturer.roman);
+    const romanSpan = document.createElement('span');
+    romanSpan.setAttribute('class', 'lecturer-profile-text');
+    romanSpan.appendChild(roman);
+
+    const name = document.createTextNode(lecturer.name);
+    const nameSpan = document.createElement('span');
+    nameSpan.setAttribute('class', 'lecturer-profile-name');
+    nameSpan.appendChild(name);
+
+    const belongs = document.createTextNode(lecturer.belongs);
+    const belongsSpan = document.createElement('span');
+    belongsSpan.setAttribute('class', 'lecturer-profile-text');
+    belongsSpan.appendChild(belongs);
+    
+    const div = document.createElement('div');
+    div.appendChild(img);
+    div.appendChild(romanSpan);
+    div.appendChild(nameSpan);
+    div.appendChild(belongsSpan);
+
+    const career = document.createTextNode(lecturer.position + '  ' + lecturer.career);
+    const p = document.createElement('p');
+    p.appendChild(career);
+
+    const li = document.createElement('li');
+    li.setAttribute('class', 'lecturer-profile-li');
+    li.appendChild(div);
+    li.appendChild(p);    
+    
+    return li;
 }
